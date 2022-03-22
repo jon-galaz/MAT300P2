59db8c01a699612dddad51b613950c96f9f3ba4a
Jon Galaz <jon.galaz@digipen.edu>
2022-03-22 20:22:07 +0100

    Improvements

--- cubicspline.m
+++ cubicspline.m
@@ -13,14 +13,37 @@ function cubicspline
   
   Ts = linspace(0, n, n+1);
   outMesh = linspace(0, n, outputNodes);
+  Ps = ones(1, n + 2);
   
-  matrix = zeros(n + 2, n + 3);
-  
-  
-  for i = 1 : n
-    for j = 1 : n + 3
-      
+  #matrix for x and y 
+  if(dimension == 2)
+    matrix = zeros(n + 2, n + 4);
+    
+    #rows
+    for i = 1 : n
+      #columns
+      for j = 1 : n + 2
+        if(j <=4 )
+          matrix(i, j) = Ps(1,j) * Ts(1, i)^(j-1);
+        else
+          matrix(i, j) = Ps(1,j) * max(0,(Ts(1, i) - Ts(1, j - 3))^3);
+        endif
+      endfor
     endfor
-  endfor
+    
+    #"hardcode" the x and y values
+    matrix(:, n+3) = resize(PX,1, n+2);
+    matrix(:, n+4) = resize(PY,1, n+2);
+    
+    #"hardcode" derivatives
+    matrix
+    
+  #matrix for x, y and z
+  else
+    matrix = zeros(n + 2, n + 5);
+  endif
+  
+  
+  
   
   endfunction
